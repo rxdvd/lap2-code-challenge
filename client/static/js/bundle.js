444b3b5da0f45c9ded39e7aed70adca7a01c91d4
ewhite1999 <ewhite1999@gmail.com>
2022-02-16 12:01:05 +0000

    add bundler

--- client/static/js/bundle.js
+++ client/static/js/bundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const { handleSubmit } = require("./helpers");
 
 // A function to generate the text input for posts
@@ -48,3 +49,18 @@ const createForm = () => {
 };
 
 module.exports = { createForm };
+
+},{"./helpers":2}],2:[function(require,module,exports){
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(e);
+};
+
+module.exports = { handleSubmit };
+
+},{}],3:[function(require,module,exports){
+const { createForm } = require("./form");
+
+createForm();
+
+},{"./form":1}]},{},[3]);
