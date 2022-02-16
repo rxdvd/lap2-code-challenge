53d54a5eec8705daf27cbc4eb78d8d0a300eabdb
Wai Lon <rxdvd0@gmail.com>
2022-02-16 11:13:30 +0000

    fix tests and use test server

--- api/test/posts.spec.js
+++ api/test/posts.spec.js
@@ -1,29 +1,42 @@
 const request = require('supertest');
 const app = require('../server');
 const posts = require('../data/posts');
+const port = process.env.PORT || 5000;
 
 describe('/posts routes', () => {
+    let api;
+    beforeAll(() => {
+        api = app.listen(port, () =>{
+            console.log(`Test server listening on port ${port}`);
+        });
+    });
+
+    afterAll(done => {
+        console.log("Gracefully stopping test server...");
+        api.close(done);
+    });
+
     describe('GET /posts/:id', () => {
         it('returns status code 200 on success', done => {
-            request(app)
+            request(api)
                 .get("/posts/1")
                 .expect(200, done);
         });
 
         it('returns status code 404 on failure', done => {
-            request(app)
+            request(api)
                 .get("/posts/100")
                 .expect(404, done);
         });
 
         it('returns json', done => {
-            request(app)
+            request(api)
                 .get("/posts/1")
                 .expect('Content-Type', /json/, done);
         });
 
         it('returns correct response data', done => {
-            request(app)
+            request(api)
                 .get("/posts/1")
                 .expect(posts[0], done);
         });
@@ -37,7 +50,7 @@ describe('/posts routes', () => {
         };
 
         it('returns status code 201 on success', done => {
-            request(app)
+            request(api)
                 .post("/posts")
                 .send(testPost)
                 .expect(201, done);
@@ -48,34 +61,34 @@ describe('/posts routes', () => {
                 title: "test",
                 body: "test body"
             };
-            request(app)
+            request(api)
                 .post("/posts")
                 .send(noNamePost)
                 .expect(201, done);
         });
 
         it('returns status code 422 on failure', done => {
-            request(app)
+            request(api)
                 .post("/posts")
                 .send({foo: "bar"})
                 .expect(422, done);
         });
 
         it('returns json on success', done => {
-            request(app)
+            request(api)
                 .post("/posts")
                 .send(testPost)
                 .expect('Content-Type', /json/, done);
         });
 
         it('returns correct response data for success', done => {
-            request(app)
+            request(api)
                 .post("/posts")
                 .send(testPost)
                 .end((err, res) => {
                     if(err) return done(err);
                     try {
-                        expect(res.body.post_id).toBe(3);
+                        expect(res.body.post_id).toBe(6);
 
                         expect(res.body).toMatchObject(testPost);
 
