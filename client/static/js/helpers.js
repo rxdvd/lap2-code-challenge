2685cec7342e8d36cd46a28ba77c083dfd2de932
ewhite1999 <ewhite1999@gmail.com>
2022-02-16 12:26:27 +0000

    make form elems required

--- client/static/js/helpers.js
+++ client/static/js/helpers.js
@@ -1,6 +1,11 @@
-const handleSubmit = (e) => {
+const postStory = (e) => {
   e.preventDefault();
-  console.log(e);
+  console.log(e.target);
+  const title = e.target.querySelector("#form_title").value;
+  const name = e.target.querySelector("#form_name").value;
+  const story = e.target.querySelector("#form_story").value;
+
+  console.log(title, name, story);
 };
 
-module.exports = { handleSubmit };
+module.exports = { postStory };
